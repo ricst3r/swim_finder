4748fb9a451d0c02b30d74aa8eb4b687a596032c
Jaimi Klein <jaimiblankenship@gmail.com>
2024-09-06 15:11:13 +0200

    Revert "Fridaytask"

--- app/models/amenity.rb
+++ app/models/amenity.rb
@@ -1,12 +1,3 @@
 class Amenity < ApplicationRecord
   has_many :reviews, through: :review_amenities
-  def icon
-    case title.downcase
-    when 'wifi' then 'wifi'
-    when 'parking' then 'p-square'
-    when 'pool' then 'water'
-    # Add more mappings as needed
-    else 'check-circle'  # Default icon
-    end
-  end
 end
