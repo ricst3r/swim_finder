904b80855c2e6a0fbd473476ff4e323a092070a0
Ermira Guri <ere.guri@gmail.com>
2024-09-06 15:30:30 +0200

    checkboxes fixed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -11,11 +11,18 @@ def create
   @location = Location.find(params[:location_id])
   @review.location = @location
   @review.user = current_user
-  @review.save
-  review_params[:amenity_ids].each do |amenity_id|
-    ReviewAmenity.create(review: @review, amenity_id: amenity_id)
+
+  if @review.save
+    # Only process amenities if they are present
+    if params[:review][:amenity_ids].present?
+      params[:review][:amenity_ids].each do |amenity_id|
+        ReviewAmenity.create(review: @review, amenity_id: amenity_id)
+      end
+    end
+    redirect_to location_path(@location), notice: 'Review was successfully created.'
+  else
+    render :new
   end
-  redirect_to location_path(@location)
 end
 
 private
