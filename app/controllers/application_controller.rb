c4770510f7aa49bcfbd5758922c6651e1a9b1319
Lutz Ricardo Hirschbiegel @users.noreply.github.com <l.r_hirschbiegel@hotmail.de>
2024-09-11 15:36:41 +0200

    bugfix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options
-    { host: ENV[“DOMAIN”] || “localhost:3000” }
+    { host: ENV["DOMAIN"] || "localhost:3000" }
   end
 
 end
